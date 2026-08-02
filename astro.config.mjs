fa4cf6e07530199b9accf7640e03fe0d83a312a7
mattecdao <jecdao@ucsd.edu>
2026-08-01 22:17:10 -0700

    Update sitemap integration with entry limit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: "https://cse134b-hw5-genesite.netlify.app",
-  integrations: [sitemap()]
+  integrations: [sitemap({
+    entrylimit: 10000,
+    )],
 });
